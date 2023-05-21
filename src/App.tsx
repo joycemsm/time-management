c239bd71487e6b81d8a70677d28c6236da656f53
Joyce <joycemara.sm@gmail.com>
2023-05-22 01:14:37 +0200

    created some configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello</h1>
-    </>
-  );
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
