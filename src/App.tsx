b097e9c629a432ad9bbc03647f57f0a80b63e2a2
Joyce <joycemara.sm@gmail.com>
2023-05-23 23:56:22 +0200

    created route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { GlobalStyle } from './styles/global'
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello</h1>
+      <BrowserRouter />
+      <Router />
+      <BrowserRouter />
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
