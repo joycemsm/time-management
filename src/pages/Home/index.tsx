9de58694ce66b4364211d48d0d4c6754e4703a8a
Joyce <joycemara.sm@gmail.com>
2023-06-01 23:55:57 +0200

    reduced countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,10 +9,11 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
+import { useEffect, useState } from "react";
 
 import { Play } from "phosphor-react";
+import { differenceInSeconds } from "date-fns";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const newCycleFormValidationSchema = zod.object({
@@ -26,7 +27,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
-  isActive: boolean;
+  startDate: Date;
 }
 
 export function Home() {
@@ -34,14 +35,26 @@ export function Home() {
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
-  const { register, handleSubmit, watch, reset, formState } =
-    useForm<NewCycleFormData>({
-      resolver: zodResolver(newCycleFormValidationSchema),
-      defaultValues: {
-        task: "",
-        minutesAmount: 0,
-      },
-    });
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(
+    (activeCycle) => {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    },
+    [activeCycle]
+  );
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -50,6 +63,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
     setCycles((state) => [...state, newCycle]);
@@ -57,8 +71,6 @@ export function Home() {
 
     reset();
   }
-
-  const activeCycle = cycles.find((cycle) => cycles.id === activeCycleId);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
